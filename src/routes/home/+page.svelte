d4ea3d2192b74382362bac5a4e6bd5a5f0a00e65
ather <rizviather6@gmail.com>
2024-06-22 14:09:41 -0400

    Adding test folders and shad cn

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '@/components/ui/button';
+	import * as Drawer from '$lib/components/ui/drawer';
+</script>
+
+<Button>Click me</Button>
+<Drawer.Root>
+	<Drawer.Trigger>Open</Drawer.Trigger>
+	<Drawer.Content>
+		<Drawer.Header>
+			<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+			<Drawer.Description>This action cannot be undone.</Drawer.Description>
+		</Drawer.Header>
+		<Drawer.Footer>
+			<Button>Submit</Button>
+			<Drawer.Close>Cancel</Drawer.Close>
+		</Drawer.Footer>
+	</Drawer.Content>
+</Drawer.Root>
